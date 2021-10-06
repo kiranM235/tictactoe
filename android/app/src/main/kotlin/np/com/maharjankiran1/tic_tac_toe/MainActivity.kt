4bebc70d923ffd6ee379f4a96dbc611d2aab8169
KiranMaharjan23 <kiranmaharjan894@outlook.com>
2021-10-06 18:37:00 +0545

    tic tac toe game

---
+++ android/app/src/main/kotlin/np/com/maharjankiran1/tic_tac_toe/MainActivity.kt
@@ -0,0 +1,6 @@
+package np.com.maharjankiran1.tic_tac_toe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:tic_tac_toe/src/homepage.dart';
+
+void main()=> runApp(MaterialApp(
+  title: "Tic Tac Toe",
+  debugShowCheckedModeBanner: false,
+  home: HomePage(),
+));
+
+
